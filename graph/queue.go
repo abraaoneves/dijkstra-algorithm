ae41346cbd207c86f450c3b5d167464729b097a2
Abraao Neves <6154363+abraaoneves@users.noreply.github.com>
2022-05-22 17:02:55 -0300

    Add enqueue method for graph queue algorithms

--- graph/queue.go
+++ graph/queue.go
@@ -8,3 +8,39 @@ type NodeQueue struct {
 	Items []Vertex
 	Lock  sync.RWMutex
 }
+
+// Enqueue adds an Node to the end of the queue
+func (queue *NodeQueue) Enqueue(vertex Vertex) {
+	queue.Lock.Lock()
+
+	// add first value to the queue
+	if len(queue.Items) == 0 {
+		queue.Items = append(queue.Items, vertex)
+		queue.Lock.Unlock()
+		return
+	}
+
+	var isInserted bool
+	for key, value := range queue.Items {
+		if vertex.Distance < value.Distance {
+			if key > 0 {
+				queue.Items = append(queue.Items[:key+1], queue.Items[key:]...)
+				queue.Items[key] = vertex
+				isInserted = true
+			} else {
+				queue.Items = append([]Vertex{vertex}, queue.Items...)
+				isInserted = true
+			}
+		}
+
+		if isInserted {
+			break
+		}
+	}
+
+	if !isInserted {
+		queue.Items = append(queue.Items, vertex)
+	}
+
+	queue.Lock.Unlock()
+}
